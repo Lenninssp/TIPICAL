f9c6ce8d91d62adf064ea87f62a9492f0dadaac4
Lennin sabogal <lenninssp1021@gmail.com>
2026-02-04 22:03:05 -0500

    first post method

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,53 @@
-var admin = require('firebase-admin')
-var serviceAccount = require('./tipical-bd8e7-firebase-adminsdk-fbsvc-b0a76b6eb9.json')
+import { getAuth } from "firebase-admin/auth";
+import createUser from "./src/tests/create-user.html"; 
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./tipical-bd8e7-firebase-adminsdk-fbsvc-b0a76b6eb9.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const server = Bun.serve({
   port: 3000,
   routes: {
-    "/": () => new Response('Hello'),
+    "/": () => new Response("Hello"),
     "/initialize": () => {
       try {
         const db = admin.firestore();
-        return new Response("Firestore initialized ok")
+        return new Response("Firestore initialized ok");
       } catch (err) {
-        return new Response(`Firestore did not initialize ok: ${err}`);
+        return new Response(`Firestore did not initialize: ${err}`);
       }
-    }
-  }
-})
+    },
+    "/get-user": () => {
+      try {
+        getAuth()
+          .getUser("0")
+          .then((userRecord) => {
+            return new Response(
+              `Successfully fetched the user data: ${userRecord.toJSON()}`,
+            );
+          })
+          .catch((error) => {
+            return new Response(`Error fetching user data: ${error}`);
+          });
+        return new Response(
+          "If you only see this then it simply dodn't find an user",
+        );
+      } catch (err) {
+        return new Response(`Error getting the user information: ${err}`);
+      }
+    },
+    "/create-user": {
+      POST: async (req) => {
+        const body: any = await req.json();
+        return Response.json({ created: true, message: "you are sexy lennin", ...body });
+      },
+    },
+  },
+});
 
-console.log(`Listening on ${server.url}`)
+console.log(`Listening on ${server.url}`);
 
 console.log("Hello via Bun!");
