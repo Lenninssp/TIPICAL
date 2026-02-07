f9cf7f169ecc8b518a9ddcd097e1a90f6ac05f53
Lennin sabogal <lenninssp1021@gmail.com>
2026-02-07 15:45:13 -0500

    Fucking firebase database doesn't connect

--- backend/src/features/posts/models/postSchema.ts
+++ backend/src/features/posts/models/postSchema.ts
@@ -7,14 +7,14 @@ export const SelectPostSchema = createSelectSchema(PostTable);
 export const CreatePostSchema = createInsertSchema(PostTable, {
   title: z.string().min(1).trim(),
   description: z.string().min(1),
-  latitude: z.number().min(-90).max(90).optional(),
-  longitude: z.number().min(-180).max(180).optional(),
 }).omit({
   id: true,
   ownerId: true,
   createdAt: true,
   editedAt: true,
   archived: true,
+  latitude: true,
+  longitude: true,
 });
 
 export const UpdateTaskSchema = CreatePostSchema.partial();
@@ -26,8 +26,8 @@ export const PostSchema = z
       id: "123e4567-e89b-12d3-a456-426614174000",
       title: "Sample Post",
       description: "This is a sample description for the post.",
-      createdAt: "2023-01-01T00:00:00Z",
-      editedAt: "2023-01-02T00:00:00Z",
+      createdAt: 1738944000000,
+      editedAt: 1738944000000,
       ownerId: "456e7890-e12b-34d5-a678-526614174001",
       archived: false,
       latitude: 40.7128,
