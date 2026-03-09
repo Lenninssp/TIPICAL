77286250e8704881f758cc3bbadeb51b9534eb10
Sofia Guerra <sofiaguerraquintero@gmail.com>
2026-03-09 19:41:22 -0400

    Fixed profile colors, like cards done, post model done, create post done, mapkit added but solve coordenates problem, search view created, side menu fixed

---
+++ frontend-ios/frontend-ios/Models/Like.swift
@@ -0,0 +1,8 @@
+//
+//  Like.swift
+//  frontend-ios
+//
+//  Created by Sofia Guerra on 2026-03-09.
+//
+
+import Foundation
---
+++ frontend-ios/frontend-ios/Views/MapPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  MapPickerView.swift
+//  frontend-ios
+//
+//  Created by Sofia Guerra on 2026-03-09.
+//
+
+import SwiftUI
+
+struct MapPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapPickerView()
+}
